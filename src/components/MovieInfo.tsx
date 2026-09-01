ddfe2134ca81ef76e942cf370e25fc83a523ddef
irensm27 <semenets.irina27@gmail.com>
2026-09-01 20:21:15 +0300

    store+services+ start components

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -1,8 +1,22 @@
+import type {IMovie} from "../models/IMovie.ts";
+import type {IGenre} from "../models/IGenre.ts";
+import type {FC} from "react";
+import GenreBadge from "./GenreBadge.tsx";
 
+interface IMovieInfoProps {
+    movie: IMovie;
+    genres:IGenre[];
+}
 
-const MovieInfo = () => {
+const MovieInfo:FC<IMovieInfoProps> = ({movie, genres}) => {
+    const movieGenres = genres.filter(genre=> movie.genre_ids.includes(genre.id))
     return (
         <div>
+            {
+                movieGenres.map(genre=><GenreBadge genre={genre} key={genre.id}/>)
+            }
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
 
         </div>
     );
--- src/components/MovieListCard.tsx
+++ src/components/MovieListCard.tsx
@@ -2,8 +2,9 @@ import type {IMovie} from "../models/IMovie.ts";
 import type {FC} from "react";
 import PosterPreview from "./PosterPreview.tsx";
 import StarsRating from "./StarsRating.tsx";
+import MovieInfo from "./MovieInfo.tsx";
 import type {IGenre} from "../models/IGenre.ts";
-import GenreBadge from "./GenreBadge.tsx";
+
 
 type MovieListCardProps = {
     movie: IMovie;
@@ -11,18 +12,21 @@ type MovieListCardProps = {
 }
 
 const MovieListCard:FC<MovieListCardProps> = ({movie, genres}) => {
-    const movieGenres = genres.filter(genre=> movie.genre_ids.includes(genre.id))
     return (
         <div>
             <PosterPreview posterPath={movie.poster_path} title={movie.title}/>
-            {
-                movieGenres.map(genre=><GenreBadge genre={genre} key={genre.id}/>)
-            }
             <StarsRating rating={movie.vote_average}/>
-            <h2>{movie.title}</h2>
-            <p>{movie.overview}</p>
+            <MovieInfo movie={movie} genres={genres}/>
             <p>{movie.release_date}</p>
             <p>{movie.vote_average}</p>
+            <p>Votes: {movie.vote_count}</p>
+            <p>Language: {movie.original_language}</p>
+            <p>Popularity: {movie.popularity}</p>
+
+            {
+                movie.original_title !== movie.title &&
+                <p>Original title: {movie.original_title}</p>
+            }
         </div>
     );
 };
